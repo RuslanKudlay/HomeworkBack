1edb45d2b55f412267055daa58bda3a22ea7e779
kudlay.rusik@gmail.com <kudlay.rusik@gmail.com>
2021-07-16 08:12:45 +0300

    Add project files.

---
+++ HomeWorkBack/Controllers/WeatherForecastProdController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastProdController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastProdController> _logger;
+
+        public WeatherForecastProdController(ILogger<WeatherForecastProdController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var msg = "WeatherForecastProdController";
+            return new JsonResult(msg);
+        }
+
+    }
+}
